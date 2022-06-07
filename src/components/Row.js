30e7d2dc1869d625071e328abfaf1a646a8534c9
Tolga Ergin <tolgaergin@gmail.com>
2022-06-08 00:02:29 +0100

    back to original (px to rem)

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,9 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import config, { DIMENSION_NAMES } from '../config';
 
-import config, { DIMENSION_NAMES } from '../config'
-
-const ModificatorType = PropTypes.oneOf(DIMENSION_NAMES)
+const ModificatorType = PropTypes.oneOf(DIMENSION_NAMES);
 
 const Row = styled.div`
   box-sizing: border-box;
@@ -12,55 +11,77 @@ const Row = styled.div`
   flex: 0 1 auto;
   flex-direction: row;
   flex-wrap: wrap;
-  margin-right: ${p => config(p).gutterWidth / 2 * -1}px;
-  margin-left: ${p => config(p).gutterWidth / 2 * -1}px;
+  margin-right: ${(p) => (config(p).gutterWidth / 2) * -1}rem;
+  margin-left: ${(p) => (config(p).gutterWidth / 2) * -1}rem;
 
-  ${p => p.reverse && `
+  ${(p) =>
+    p.reverse &&
+    `
     flex-direction: row-reverse;
   `}
 
-  ${p => p.start && config(p).media[p.start]`
+  ${(p) =>
+    p.start &&
+    config(p).media[p.start]`
     justify-content: flex-start;
   `}
 
-  ${p => p.center && config(p).media[p.center]`
+  ${(p) =>
+    p.center &&
+    config(p).media[p.center]`
     justify-content: center;
   `}
 
-  ${p => p.end && config(p).media[p.end]`
+  ${(p) =>
+    p.end &&
+    config(p).media[p.end]`
     justify-content: flex-end;
   `}
 
-  ${p => p.top && config(p).media[p.top]`
+  ${(p) =>
+    p.top &&
+    config(p).media[p.top]`
     align-items: flex-start;
   `}
 
-  ${p => p.middle && config(p).media[p.middle]`
+  ${(p) =>
+    p.middle &&
+    config(p).media[p.middle]`
     align-items: center;
   `}
 
-  ${p => p.bottom && config(p).media[p.bottom]`
+  ${(p) =>
+    p.bottom &&
+    config(p).media[p.bottom]`
     align-items: flex-end;
   `}
 
-  ${p => p.around && config(p).media[p.around]`
+  ${(p) =>
+    p.around &&
+    config(p).media[p.around]`
     justify-content: space-around;
   `}
 
-  ${p => p.between && config(p).media[p.between]`
+  ${(p) =>
+    p.between &&
+    config(p).media[p.between]`
     justify-content: space-between;
   `}
 
-  ${p => p.first && config(p).media[p.first]`
+  ${(p) =>
+    p.first &&
+    config(p).media[p.first]`
     order: -1;
   `}
 
-  ${p => p.last && config(p).media[p.last]`
+  ${(p) =>
+    p.last &&
+    config(p).media[p.last]`
     order: 1;
   `}
-`
+`;
 
-Row.displayName = 'Row'
+Row.displayName = 'Row';
 
 Row.propTypes = {
   reverse: PropTypes.bool,
@@ -74,7 +95,7 @@ Row.propTypes = {
   between: ModificatorType,
   first: ModificatorType,
   last: ModificatorType,
-  children: PropTypes.node
-}
+  children: PropTypes.node,
+};
 
-export default Row
+export default Row;
